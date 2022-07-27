dc787aba93dbee3571edda921d9772346a64925b
Tiago Mello <tiagomellobr@gmail.com>
2022-07-26 23:07:31 -0300

    feat: :sparkles: create a socket server

--- src/infra/api/ExpressAdapter.ts
+++ src/infra/api/ExpressAdapter.ts
@@ -1,13 +1,68 @@
 import express, { Request, Response } from "express";
 import HttpServer from "./HttpServer";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 export default class ExpressAdapter implements HttpServer {
 
     app: any;
+    server: any;
+    io: any;
 
     constructor() {
         this.app = express();
         this.app.use(express.json());
+        this.app.use(cors());
+
+        this.server = http.createServer(this.app);
+
+        const origin = process.env.CLIENT_CORS_URL ?? '*';
+
+        this.io = new Server(this.server, {
+            cors: {
+                origin,
+                methods: ["GET", "POST"]
+            }
+        });
+
+        let users: Array<any> = [];
+
+        const addUser = (userId: string, socketId: string) => {
+            !users.some((user) => user.userId === userId) &&
+                users.push({ userId, socketId });
+        };
+
+        const removeUser = (socketId: string) => {
+            users = users.filter((user) => user.socketId !== socketId);
+        };
+
+        const getUser = (userId: string) => {
+            return users.find((user) => user.userId === userId);
+        };
+
+        this.io.on("connection", (socket: any) => {
+            console.log(`User connected (${socket.id})`);
+
+            socket.on("addUser", (userId: string) => {
+                addUser(userId, socket.id);
+                this.io.emit("getUsers", users);
+            });
+
+            socket.on("sendMessage", ({ senderId, receiverId, text }: any) => {
+                const user = getUser(receiverId);
+                this.io.to(user.socketId).emit("getMessage", {
+                    senderId,
+                    text,
+                });
+            });
+
+            socket.on("disconnect", () => {
+                console.log("User disconnected!");
+                removeUser(socket.id);
+                this.io.emit("getUsers", users);
+            });
+        });
     }
 
     on(method: string, url: string, callback: Function): void {
@@ -17,7 +72,8 @@ export default class ExpressAdapter implements HttpServer {
         });
     }
     listen(port: number): void {
-        this.app.listen(port);
+        this.server.listen(port);
+        console.log(`SERVER IS RUNNING - PORT:${port}`);
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import ExpressAdapter from "./infra/api/ExpressAdapter";
 import Router from "./infra/api/Router";
 import ConversationMemoryRepository from "./infra/repository/ConversationMemoryRepository";
 import MessageMemoryRepository from "./infra/repository/MessageMemoryRepository";
+import dotenv from "dotenv";
+import 'dotenv/config';
+
+dotenv.config({ path: __dirname+'/.env' });
 
 const conversationRepository = new ConversationMemoryRepository();
 const messageRepository = new MessageMemoryRepository();
@@ -9,4 +13,7 @@ const messageRepository = new MessageMemoryRepository();
 const httpServer = new ExpressAdapter();
 const router = new Router(httpServer, conversationRepository, messageRepository);
 router.init();
-httpServer.listen(3000);
+
+const port = parseInt(process.env.APP_PORT || '8008');
+
+httpServer.listen(port);
